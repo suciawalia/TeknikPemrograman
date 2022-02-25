4798daf7f2477b8a6d5002009277fffb9f2ce4f7
Suci Awalia Gardara <99374578+suciawalia@users.noreply.github.com>
2022-02-25 21:46:08 +0700

    Add files via upload

---
+++ Pratikum1/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package constants;
+
+/**
+ *
+ * @author X1 Carbon
+ */
+public class Constants {
+ public static void main (String[] args){
+     final double CM_PER_INCH = 2.54;
+     double paperWidth = 8.5;
+     double paperHeight = 11;
+     System.out.println("Paper size in centimeters: " + paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+ }
+}
