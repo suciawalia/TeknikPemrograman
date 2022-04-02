f90ff74f6342671c50f597d4e67a3cfaef301a0f
Suci Awalia Gardara <99374578+suciawalia@users.noreply.github.com>
2022-04-02 21:33:14 +0700

    Add files via upload

---
+++ W8/shape/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shape;
+
+/**
+ *
+ * @author Suci Awalia Gardara
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        Rectangle rtgl = new Rectangle();
+        Square square = new Square();
+        System.out.println(rtgl.toString());
+        System.out.println(square.toString());
+    }
+}
+
