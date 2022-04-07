7b5488a01a9c68dd8e1bea8ebea72489f98ce936
Suci Awalia Gardara <99374578+suciawalia@users.noreply.github.com>
2022-04-07 08:39:52 +0700

    Add files via upload

---
+++ W7/ATMMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package account;
+
+/**
+ *
+ * @author Suci Awalia Gardara
+ */
+
+
+ 
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
