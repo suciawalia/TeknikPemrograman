42c3aa0b601502d4ba3c3de3ab75d621f7ebecff
Suci Awalia Gardara <99374578+suciawalia@users.noreply.github.com>
2022-04-07 08:37:07 +0700

    Add files via upload

---
+++ W7/Screen.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package account;
+
+/**
+ *
+ * @author User
+ */
+
+
+// Screen.java
+// Represents the screen of the ATM
+
+public class Screen
+{
+    public void displayMessage( String message )
+    {
+        System.out.print( message );
+    }
+   
+    public void displayMessageLine( String message )
+    {
+        System.out.println( message );
+    }
+   
+    public void displayDollarAmount( double amount )
+    {
+        System.out.printf( "$%,.2f", amount );
+    }
+   
+}
